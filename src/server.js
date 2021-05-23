09f43b1ef7cb5e8a2af4d53eb8720766ded8025b
Hewerton Moreira <79805868+hewmoreira@users.noreply.github.com>
2021-05-23 12:28:29 -0300

    incluindo map em repos e filtrando os atributos

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const axios = require('axios')
 const express = require ('express')
 const app = express()
 const url = 'https://api.github.com/orgs/takenet/repos?sort=created&direction=asc'
+const maxRepos = 5
 
 const language = f => f.language === 'C#'
 
@@ -9,11 +10,12 @@ app.get('/', async (req, res) => {
 
     try {
         const { data } = await axios(url)
-        
+        const getMappedValues = ({ name, owner:{avatar_url}, description, created_at, language }) => ({ name, owner:{avatar_url}, created_at, description, language });
         const repos = data
             .filter(language)
-            .slice(0, 5)
-        
+            .slice(0, maxRepos)
+            .map(getMappedValues)
+
         return res.json(repos)
     } catch (error) {
         console.error(error)
